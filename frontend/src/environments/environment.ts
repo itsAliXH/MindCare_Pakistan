614a142005fe5f5166e3b87e44ae24a8c73a0147
Ali <itsalixh@gmail.com>
2025-10-06 23:14:05 +0500

    updated API URL

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api',
+  // apiUrl: 'http://localhost:4000/api',
+  _apiUrl: 'https://mindcare-eu8j.onrender.com',
+  get apiUrl() {
+    return this._apiUrl;
+  },
+  set apiUrl(value) {
+    this._apiUrl = value;
+  },
   appTitle: 'MindCare Pakistan',
   version: '1.0.0'
 };
